ea459dd6608d50beab73d1c47e3d31502d60dfb2
spaceboy <woobin0413@gmail.com>
2019-02-23 20:40:16 -0600

    final commit before extension project

---
+++ parsed_data/models.py
@@ -0,0 +1,30 @@
+# parsed_data/models.py
+# from django.db import models
+
+
+# class BlogData(models.Model):
+#     title = models.CharField(max_length=200)
+#     link = models.URLField()
+#
+#     def __str__(self):
+#         return self.title
+
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
---
+++ parser.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "websaver.settings")
+import django
+django.setup()
+# BlogData를 import해옵니다
+from parsed_data.models import BlogData
+
+def parse_blog():
+    req = requests.get('https://beomi.github.io/beomi.github.io_old/')
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    my_titles = soup.select(
+        'h3 > a'
+        )
+    data = {}
+    for title in my_titles:
+        data[title.text] = title.get('href')
+    return data
+
+# 이 명령어는 이 파일이 import가 아닌 python에서 직접 실행할 경우에만 아래 코드가 동작하도록 합니다.
+if __name__=='__main__':
+    blog_data_dict = parse_blog()
+    for t, l in blog_data_dict.items():
+        BlogData(title=t, link=l).save()
